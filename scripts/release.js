64317f022ee9d7329a0b54bbbe5fd14e5e751fce
ImOverlord <thibault-alexandre.faure@epitech.eu>
2019-06-19 19:20:48 +0200

    Fixed an issue with CI no triggering release build, Updated release script to print what it is doing

--- scripts/release.js
+++ scripts/release.js
@@ -4,5 +4,9 @@ const execSync = require("child_process").execSync;
 const version = package.version;
 const name    = package.name;
 
+console.log(`Publishing ${name} v${version}`);
+console.log(`Adding latest tag`);
 execSync(`npm dist-tag add ${name}@${version} latest`);
+console.log(`Removing beta tag`);
 execSync(`npm dist-tag rm ${name}@${version} beta`);
+console.log(`${name} was published`);
